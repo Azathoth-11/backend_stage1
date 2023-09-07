7323a2ec081a1dd11c9e7be456d56a19e65148e2
Rohit Sinha <rohitsinha5711@gmail.com>
2023-09-07 19:17:08 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
     return {'message': 'Hello, World!'}
+
+def handler(request: Request):
+    return app(request)
 
 if __name__ == '__main__':
     app.run(debug=True)
